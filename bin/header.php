83a5cce22aaef174cb6aea3d72296d3056bdf621
Chris <114068287+chris-fweather@users.noreply.github.com>
2022-12-02 18:09:09 +0800

    Create header.php

---
+++ bin/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
+    <title>!smB | Regiment</title>
+    <link rel="stylesheet" href="./styles/christmas.css?<?php echo time(); ?>" />
+    <script src="../js/snow.js"></script>
+    <script src="../js/index.js"></script>
+    <script src="https://kit.fontawesome.com/d2256f7f6b.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <ul class="nav__ul--main">
+            <figure>
+                <img class="nav__logo" src="../assets/smb-logo-xmas.png">
+            </figure>
+            <li class="nav__li">
+                <a class="nav__a underline" href="../index.php">HOME</a>
+            </li>
+            <li class="nav__li">
+                <a class="nav__a underline" href="#">ROSTER</a>
+            </li>
+            <li class="nav__li">
+                <a class="nav__a underline" href="../about.php">ABOUT US</a>
+            </li>
+            <li class="nav__li">
+                <a class="nav__a underline" href="#">CONTACT US</a>
+            </li>
+        </ul>
+        <ul class="nav__ul--login">
+            <li class="nav__li">
+                <a class="nav__a underline" href="../login.php">LOGIN</a>
+            </li>
+        </ul>
+        <ul id="nav__ul--mobile">
+            <figure>
+                <img class="nav__logo" src="../assets/smb-logo.png">
+            </figure>
+            <li>
+                <a href="./navbar.php"><i class="fa fa-bars"></i></a>
+            </li>
+        </ul>
+    </nav>
