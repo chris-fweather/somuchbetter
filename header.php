a7d9a803bc1850a2a1fab8c7a1b5240972073f39
Chris <114068287+chris-fweather@users.noreply.github.com>
2023-01-04 01:42:03 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    require_once './includes/session.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
     <title>!smB | Regiment</title>
-    <link rel="stylesheet" href="./styles/christmas.css?<?php echo time(); ?>" />
-    <script src="./js/snow.js"></script>
+        <!-- CHECK CSS STYLES FOR SEASONS -->
+    <link rel="stylesheet" href="./styles/main.css?<?php echo time(); ?>" />
+        <!-- ACTIVATE ON CSS FILE -->
+    <!-- <script src="./js/snow.js"></script> -->
     <script src="./js/index.js"></script>
     <script src="https://kit.fontawesome.com/d2256f7f6b.js" crossorigin="anonymous"></script>
 </head>
@@ -19,25 +21,57 @@
     <nav>
         <ul class="nav__ul--main">
             <figure>
-                <img class="nav__logo" src="./assets/smb-logo-xmas.png">
+                <img class="nav__logo" src="./assets/smb-logo.png">
             </figure>
             <li class="nav__li">
                 <a class="nav__a underline" href="./index.php">HOME</a>
             </li>
             <li class="nav__li">
-                <a class="nav__a underline" href="#">ROSTER</a>
+                <a class="nav__a underline" href="./roster.php">ROSTER</a>
             </li>
             <li class="nav__li">
                 <a class="nav__a underline" href="./about.php">ABOUT US</a>
             </li>
             <li class="nav__li">
-                <a class="nav__a underline" href="#">CONTACT US</a>
+                <a class="nav__a underline" href="./contact.php">CONTACT US</a>
             </li>
         </ul>
+        <script> 
+            function openMenu() {
+            document.body.classList += " menu--open"
+            }
+
+            function closeMenu() {
+            document.body.classList.remove('menu--open')
+            }
+        </script>
+        <button class="btn__menu" onclick="openMenu()">
+          <img class="nav__logo" src="./assets/smb-logo.png">
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="menu__backdrop">
+          <button class="btn__menu btn__menu--close" onclick="closeMenu()">
+            <i class="fas fa-times"></i>
+          </button>
+          <ul class="menu__links">
+            <li class="menu__list">
+              <a href="./index.php" class="menu__link" onclick="closeMenu()">HOME</a>
+            </li>
+            <li class="menu__list">
+              <a href="./roster.php" class="menu__link" onclick="closeMenu()">ROSTER</a>
+            </li>
+            <li class="menu__list">
+              <a href="./about.php" class="menu__link" onclick="closeMenu()">ABOUT US</a>
+            </li>
+            <li class="menu__list">
+              <a href="./contact.php" class="menu__link" onclick="closeMenu()">CONTACT US</a>
+            </li>
+          </ul>
+        </div>
         <ul class="nav__ul--login">
             <?php
                 if (isset($_SESSION['useruid'])) {
-                    echo '<li class="nav__li"><a class="nav__a underline" href="./index.php">ADMIN</a></li>';
+                    echo '<li class="nav__li"><a class="nav__a underline" href="./admin.php">ADMIN</a></li>';
                     echo '<li class="nav__li"><a class="nav__a underline" href="./includes/logout.inc.php">LOGOUT</a></li>';
                 }
                 else {  
